48be23e235403e5e7eac0e6873e6a01f91afc44b
MahHossam <mahmoudhossam.eg@outlook.com>
2018-12-15 19:23:48 +0200

    Travel Template

---
+++ scripts/firePlugins.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+
+        //owl 
+        $(".owl-carousel").owlCarousel({
+            center: true,
+            loop: true,
+            items: 4,
+            responsive: {
+                // breakpoint from 0 up
+                0: {
+                    items: 1
+                },
+                // breakpoint from 480 up
+                480: {
+                    items: 3
+                },
+                // breakpoint from 768 up
+                768: {
+                    items: 4
+                }
+            }
+        });
+
+
+        //card slider
+        $('.card-slider').bxSlider({
+            mode:'fade',
+            pager:false,
+            controls:true
+        });
+
+        $("#dtBox").DateTimePicker();
+
+    }); // doc.ready
+});
